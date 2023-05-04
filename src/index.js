8897cea1f82fac037f39fa140728a4cd4f95d626
Ryan Hanley <camelCaseGuy@users.noreply.github.com>
2023-05-03 22:53:06 -0500

    await bundle

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-function getComponent() {
+async function getComponent() {
+  const element = document.createElement('div');
+  const { default: _ } = await import('lodash');
 
-  return import('lodash')
-    .then(({ default: _ }) => {
-      const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-      return element;
-    })
-    .catch((error) => 'An error occurred while loading the component');
+  return element;
  }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+ getComponent().then((component) => {
+   document.body.appendChild(component);
+ });
